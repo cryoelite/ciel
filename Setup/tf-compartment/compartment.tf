cae0b6a2b7635f99139905ca12af059bee8cbb6e
cryoelite <arvindsagar702@gmail.com>
2024-06-12 00:37:56 +0530

    Update

--- Setup/tf-compartment/compartment.tf
+++
@@ -1,6 +0,0 @@
-resource "oci_identity_compartment" "tf-compartment" {
-    # Required
-    compartment_id = var.tenancy_OCID
-    description = "Compartment for ValheimTF resources."
-    name = "Gen2_Compartment"
-}
--- Setup/tf-vcn/access_compartment.tf
+++
@@ -1,8 +0,0 @@
-module "access_compartment"{
-    source= "github.com/cryoelite/Valheim_TF/tf-compartment"
-    private_key = var.private_key
-    tenancy_OCID = var.tenancy_OCID
-    user_OCID = var.user_OCID
-    fingerprint = var.fingerprint
-    region = var.region
-} 
